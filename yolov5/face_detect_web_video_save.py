16071a13a19af53c0d11e4cfe9f830b52593bfcf
i-zro <izero3127@gmail.com>
2021-06-06 16:30:08 +0000

    219697

--- yolov5/face_detect_web_video_save.py
+++ yolov5/face_detect_web_video_save.py
@@ -1,12 +1,15 @@
+from flask import Flask, render_template, Response
 from flask_opencv_streamer.streamer import Streamer
 import cv2
+
+app = Flask(__name__)
 
 port = 5000
 require_login = False
 streamer = Streamer(port, require_login)
-
 # Open video device 0
 video_capture = cv2.VideoCapture(0)
+
 
 while True:
     _, frame = video_capture.read()
@@ -17,3 +20,6 @@ while True:
         streamer.start_streaming()
 
     cv2.waitKey(30)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
