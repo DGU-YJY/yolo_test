5b0d7849bf2e3caa6da032a871e0edb332ab6b3d
i-zro <dek3127@gmail.com>
2021-06-08 19:27:45 +0900

    210608

--- yolov5/detect_ex.py
+++ yolov5/detect_ex.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import argparse
 import time
 from pathlib import Path
@@ -20,16 +21,186 @@ import os
 import shutil
 import random
 
+from multiprocessing import Process
+
+import re
+import sys
+
+from google.cloud import speech
+
+import pyaudio
+from six.moves import queue
+import os
+from tts_stt import *
+from emotion2react import *
+
 from gtts import gTTS
 from time import sleep
 import pyglet
 
+detect_exit = False;
 # save detect info, [label, conf]
 detect_list = []
 time_limit = 10
 cloth_dict = {
     'shirts':"셔츠", 'pants':'바지', 'skirt':'치마', 'socks':'양말', 'hood':'후드', 'jacket':'자켓', 'tshirts':'티셔츠', 'top':'상의'
 }
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+
+class MicrophoneStream(object):
+    credential_path = "B:\i-return-315911-61ff5455ec2a.json"
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+
+
+def listen_print_loop(responses):
+    """Iterates through server responses and prints them.
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+
+        # Display the transcription of the top alternative.
+        transcript = result.alternatives[0].transcript
+
+        # Display interim results, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+        #
+        # If the previous result was longer than this one, we need to print
+        # some extra spaces to overwrite the previous result
+        overwrite_chars = " " * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + "\r")
+            sys.stdout.flush()
+            num_chars_printed = len(transcript)
+            if re.search(r"\b(날씨)\b", transcript, re.I):
+                text = TTS('오늘 서울의 날씨는 28도로 구름이 조금있어요.','./audio_stt_tts/weather.wav')
+                run_wav('./audio_stt_tts/weather.wav')
+                print("Exiting..")
+
+            # Exit recognition if any of the transcribed phrases could be
+            # one of our keywords.
+            if re.search(r"\b(끝|종료)\b", transcript, re.I):
+                print("Exiting..")
+                detect_exit = True
+                sys.exit()
+
+            num_chars_printed = 0
+
+
+def mic():
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+    language_code = "ko-KR"  # a BCP-47 language tag
+
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=language_code,
+    )
+
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (
+            speech.StreamingRecognizeRequest(audio_content=content)
+            for content in audio_generator
+        )
+
+        responses = client.streaming_recognize(streaming_config, requests)
+
+        # Now, put the transcription responses to use.
+        listen_print_loop(responses)
+
 
 @torch.no_grad()
 def detect(opt):
@@ -126,7 +297,7 @@ def detect(opt):
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    
+
                     # 디텍트한 이미지
                     if len(detect_list) < 7:
                         detect_list.append(cls)
@@ -140,15 +311,15 @@ def detect(opt):
                         img_list = os.listdir(path_img)
                         path_img = path_img+"\\"+img_list[random.randrange(0, len(img_list))]
                         color = HSV(IMG(path_img).hsv).get_color()
-                        cloth = cloth_dict[names[most]]  
+                        cloth = cloth_dict[names[most]]
                         shutil.rmtree(path_dir, ignore_errors=True)
 
                         print(color, cloth)
                         talk = color+ " "+ cloth+"입니다."
                         speak(talk)
-                        
 
-                        
+
+
 
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
@@ -199,7 +370,6 @@ def detect(opt):
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     #parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
     parser.add_argument('--weights', nargs='+', type=str, default='weights/best.pt', help='model.pt path(s)')
@@ -228,12 +398,20 @@ if __name__ == '__main__':
     parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     opt = parser.parse_args()
-    print(opt)
-    check_requirements(exclude=('tensorboard', 'pycocotools', 'thop'))
 
-    if opt.update:  # update all models (to fix SourceChangeWarning)
-        for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-            detect(opt=opt)
-            strip_optimizer(opt.weights)
-    else:
-        detect(opt=opt)
+    check_requirements(exclude=('tensorboard', 'pycocotools', 'thop'))
+    p1 = Process(target=mic)
+    p1.start()
+    p2 = Process(target=detect(opt=opt))
+    p2.start()
+
+    if detect_exit == True:
+        sys.exit()
+
+
+    # if opt.update:  # update all models (to fix SourceChangeWarning)
+    #     for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
+    #         detect(opt=opt)
+    #         strip_optimizer(opt.weights)
+    # else:
+    #     detect(opt=opt)
